07ee5a50f010453d1310a37386aa81ddf857914e
Daryl De Guzman <ragelavigne@gmail.com>
2019-09-16 21:48:47 -0800

    Added Add Todo functionality

--- js/main.js
+++ js/main.js
@@ -1,13 +1,19 @@
 var app = new Vue({
 	el: '#app',
 	data: {
-		todos: [
-				
-			]
+		todos: [],
+		addButtonVisible: true,
+		addTodoText: '',
+
 	},
 	methods: {
 		todoToggle(todo) {
 			todo.done = !todo.done;
 		},
+		addTodo() {
+			this.todos.push({'text': this.addTodoText, 'done': false});
+			this.addTodoText = '';
+			this.addButtonVisible = !this.addButtonVisible;
+		}
 	}
 });
